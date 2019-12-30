39bfb93fc23ccfb41882abbb638d35d362b15793
Oki1 <oki.archer@gmail.com>
2019-12-30 14:22:15 +0100

    added help command and made code prettier

--- control.py
+++ control.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 from sys import argv
-import os
-if(argv[1] == "-y"):
-	os.system("sudo omxplayer --loop --vol -2500 \"$(youtube-dl -f bestaudio -g ytsearch:"+argv[2]+ ")\"")
-elif(argv[1] == "-o"):
-	os.system("sudo omxplayer --vol -2500 http://proxima.shoutca.st:8357/;")
-elif(argv[1] == "-s"):
-	os.system("sudo pkill \"youtube-dl\"; sudo pkill -INT \"omxplayer\"")
+from os import system as run
+try:
+	if(argv[1] == "-y"):
+		run("sudo omxplayer --loop --vol -2500 \"$(youtube-dl -f bestaudio -g ytsearch:"+argv[2]+ ")\"")
+	elif(argv[1] == "-o"):
+		run("sudo omxplayer --vol -2500 http://proxima.shoutca.st:8357/;")
+	elif(argv[1] == "-s"): 
+		run("sudo pkill \"youtube-dl\"; sudo pkill -INT \"omxplayer\"")
+	elif(argv[1] == "-h"):
+		print("""-y + search term to play youtube video
+-o to play otvoreni radio
+-h print help(current command""")
+except:
+	print("""-y + search term to play youtube video
+-o to play otvoreni radio
+-h print help(current command""")
